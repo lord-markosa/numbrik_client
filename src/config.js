bff5d6b13773753d79b54f9dc9b20e7687d87a48
Jane Engineer <jane@microsoft.com>
2024-12-27 17:08:30 +0530

    add server url explicitly in the config.js file

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 const config = {
-    apiUrl: process.env.PROD_API_URL || process.env.DEBUG_API_URL,
+    apiUrl:
+        process.env.DEBUG_API_URL ||
+        "https://numbrik-b2fueahkf2f4bzc3.canadacentral-01.azurewebsites.net",
 };
 
 export default config;
