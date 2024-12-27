f4440f3307b66b98934840f95f6865f93f6f21ab
Jane Engineer <jane@microsoft.com>
2024-12-27 21:43:10 +0530

    fix admin token bug and enable persona card on admin's Home

--- src/pages/common/TopHeader.jsx
+++ src/pages/common/TopHeader.jsx
@@ -4,35 +4,51 @@ import { IoDiamondOutline } from "react-icons/io5";
 import { getFormattedDate } from "../../utils/getFormattedTime";
 
 import "./TopHeader.scss";
+import PersonaCard from "./PersonaCard";
 
 const TopHeader = ({
     username,
     diamonds,
-    personaClickHandler,
     customRightButton,
-    shouldShowDate = true,
     title,
+    shouldShowDate = true,
+    disablePersonaClick,
 }) => {
+    const [showPersona, setShowPersona] = React.useState(false);
+
     const currentTime = getFormattedDate(Date.now());
 
+    const getPersonaClickHandler = () => {
+        if (disablePersonaClick) return undefined;
+        return () => setShowPersona(true);
+    };
+
     return (
-        <div className="top-header">
-            <Persona
-                title={username}
-                onClick={personaClickHandler}
-                subtitle={shouldShowDate && currentTime}
-                className={"persona"}
-            />
-            {title && <div className="title">{"{" + title + "}"}</div>}
-            <div className="right-section">
-                {customRightButton || (
-                    <div className="diamonds">
-                        <IoDiamondOutline className="diamond-icon" />
-                        {diamonds}
-                    </div>
-                )}
+        <>
+            <div className="top-header">
+                <Persona
+                    title={username}
+                    onClick={getPersonaClickHandler()}
+                    subtitle={shouldShowDate && currentTime}
+                    className={"persona"}
+                />
+                {title && <div className="title">{"{" + title + "}"}</div>}
+                <div className="right-section">
+                    {customRightButton || (
+                        <div className="diamonds">
+                            <IoDiamondOutline className="diamond-icon" />
+                            {diamonds}
+                        </div>
+                    )}
+                </div>
             </div>
-        </div>
+            {showPersona && (
+                <PersonaCard
+                    closePersona={() => setShowPersona(false)}
+                    username={username}
+                />
+            )}
+        </>
     );
 };
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,15 +4,12 @@ import Topics from "../topics/Topics";
 import SearchBar from "../../components/SearchBar";
 import topicsData from "../../constants/topics.json";
 import TopHeader from "../common/TopHeader";
-import PersonaCard from "../common/PersonaCard";
 import { useSearch } from "../../hooks/useSearch";
 import { selectUserData } from "../../selectors/userSelectors";
 
 import "./Home.scss";
 
 const Home = () => {
-    const [showPersona, setShowPersona] = React.useState(false);
-
     const { username, diamonds } = useSelector(selectUserData);
 
     // incorporate this into a search hook
@@ -20,19 +17,9 @@ const Home = () => {
 
     return (
         <div className="home-page">
-            <TopHeader
-                username={username}
-                diamonds={diamonds}
-                personaClickHandler={() => setShowPersona(true)}
-            />
+            <TopHeader username={username} diamonds={diamonds} />
             <SearchBar {...searchBarProps} />
             <Topics topics={topics} />
-            {showPersona && (
-                <PersonaCard
-                    closePersona={() => setShowPersona(false)}
-                    username={username}
-                />
-            )}
         </div>
     );
 };
